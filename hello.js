0302e0cacc85a42f5c45ef4a97ace7c82055b940
vivekgorantla <23eg105j66@anurag.edu.in>
2025-03-22 11:47:51 +0530

    age feature

--- hello.js
+++ hello.js
@@ -1,12 +1,20 @@
 class GreetUser {
     username1 = "";
-    constructor(username) {
+    age=0;
+    constructor(username,age) {
         this.username1 = username;
+        this.age=age;
     }
     greet() {
         console.log(`Hello ${this.username1}`);
     }
+
+    age(){
+        console.log(`Age is ${this.age}`);
+    }
+
 }
 
 g = new GreetUser('vivek')
 g.greet()
+g,age()
